cef2d9a061f197cd24b13d7d398e4c550a016acd
meshubhangi <shubhangi.pandey08@gmail.com>
2018-07-13 22:28:28 +0530

    changes

--- vacation.js
+++ vacation.js
@@ -3,3 +3,17 @@ var greeting = document.getElementById("greeting");
 greeting.innerHTML += ", " + name;
 var enterDays = document.getElementById("enterDays");
 enterDays.onclick = calculateDays;
+function calculateDays(){
+    var daysInput = document.getElementById("days");
+    var days = daysInput.value;
+    var daysMessage = document.getElementById("daysMessage");
+     if(days < 4){
+         daysMessage.innerHTML = "Short trips are always worth it!";
+     }
+     else if(days < 7){
+         daysMessage.innerHTML = "Cool, you'll be there for almost a week!";
+     }
+     else{
+         daysMessage.innerHTML = "You'll have plenty of time to relax and have fun!";
+     }
+}
