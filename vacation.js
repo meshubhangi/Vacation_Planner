7ebbdd44dc83c3b08e4f49c2ccbb7b14ef447cd0
meshubhangi <shubhangi.pandey08@gmail.com>
2018-07-14 19:01:56 +0530

    chnges

--- vacation.js
+++ vacation.js
@@ -1,7 +1,3 @@
-var name = window.prompt("Hello! Please enter your name");
-var greeting = document.getElementById("greeting");
-greeting.innerHTML += ", " + name;
-var enterDays = document.getElementById("enterDays");
 enterDays.onclick = calculateDays;
 var daysInput = document.getElementById("days");
     var days = daysInput.value;
